f499125ea148993b1c7038b4a87ac7ba68484e7b
MrZooby <brandon.zuchowski91@gmail.com>
2024-12-22 20:20:33 -0500

    changed how search/file are set up

--- os_test.py
+++ os_test.py
@@ -7,6 +7,10 @@ import argparse
 def get_files(path):
     return os.listdir(path)
 
+def read_file(file):
+    file = open(file)
+    return file
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-p","--path", help="Enter directory to read")
 parser.add_argument("-f","--file", help="Full path of file to read")
@@ -19,14 +23,15 @@ if args.path:
     for word in list:
         print(word)
 if args.file:
-    file = open(args.file)
-    if args.search:
-        line_count = 0
-        for line in file:
-            if args.search in line:
-                line_count += 1
-                print(line, end='')
-        print(f"\n There are {line_count} matches")
-    else:
-        print(file.read())
-        file.close()
+    file = read_file(args.file)
+    print(file.read())
+if args.search:
+    if args.file is None:
+        raise Exception("-f required when using this option")
+    line_count = 0
+    file = read_file(args.file)
+    for line in file:
+        if args.search in line:
+            line_count += 1
+            print(line, end='')
+    print(f"\n There are {line_count} matches")
